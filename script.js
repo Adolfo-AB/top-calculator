5d96adf713abfdea084496e00d5ec829b9aa970e
Adolfo-AB <adolfoab6@gmail.com>
2023-02-22 18:05:07 +0100

    Create methods to update main and secondary displays (JS)

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+const MAX_CHARACTERS = 10;
+
+// Get all the buttons
+const buttons = document.querySelectorAll("button");
+const numberButtons = Array.from(buttons).filter(button => !isNaN(parseInt(button.id)));
+const operatorButtons = Array.from(buttons).filter(button => ["+", "-", "x", "%"].includes(button.id));
+const delButton = document.getElementById("DEL");
+const acButton = document.getElementById("AC");
+const equalButton = document.getElementById("=");
+const dotButton = document.getElementById(".");
+
+// Global variables to save current operands and operator
+let firstOperand = 0;
+let secondOperand = 0;
+let operator = "";
+let equalPhase = false;
+
+// Get main and secondary display
+const mainDisplay = document.getElementById("display-lower");
+const secondaryDisplay = document.getElementById("display-upper");
+
 function add(first, second) {
     return first+second;
 };
@@ -16,11 +37,40 @@ function divide(first, second) {
 
 function operate(operator, first, second) {
     return operator(first, second);
+};
+
+function updateMainDisplay(span) {
+    let currentContent = mainDisplay.textContent;
+    if (currentContent.length >= MAX_CHARACTERS) {
+        return;
+    }
+
+    const newContent = span.textContent;
+    if (newContent != 0) {
+        if (currentContent === "0") {
+            console.log("hi dad")
+            mainDisplay.textContent = newContent;
+        } else {
+            console.log("hi mum")
+            mainDisplay.textContent = currentContent + newContent;
+        };
+    };
+};
+
+function updateSecondaryDisplay(span) {
+    firstOperand = mainDisplay.textContent;
+    operator = span.textContent;
+    let currentContent = secondaryDisplay.textContent;
+    
+    secondaryDisplay.textContent = `${mainDisplay.textContent} ${operator}`;
+
 }
 
-const first = 100;
-const second = 10;
-console.log(operate(add, first, second));
-console.log(operate(subtract, first, second));
-console.log(operate(multiply, first, second));
-console.log(operate(divide, first, second));
+numberButtons.forEach(button => button.querySelector("span").addEventListener("click", event => {
+    updateMainDisplay(event.target);
+  }));
+
+  operatorButtons.forEach(button => button.querySelector("span").addEventListener("click", event => {
+    updateSecondaryDisplay(event.target)
+  }));
+
